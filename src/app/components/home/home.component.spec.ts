20c70a3a45840a68e27a129feff2e89129bd130a
Paulo Catto <paulohcatto@hotmail.com>
2025-04-18 19:14:53 -0300

    feat: Creating test for home component

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,6 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ToastService } from 'src/app/services/toast/toast.service';
+
+@Component({ selector: 'app-header', template: '' })
+class MockHeaderComponent {
+  @Output() searchResults = new EventEmitter<any>();
+}
+
+@Component({ selector: 'app-catalog', template: '' })
+class MockCatalogComponent {
+  @Input() filtered: any;
+}
+
+@Component({ selector: 'app-footer', template: '' })
+class MockFooterComponent {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,16 +25,42 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        HomeComponent,
+        MockHeaderComponent,
+        MockCatalogComponent,
+        MockFooterComponent
+      ],
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot(),
+        MatSnackBarModule
+      ],
+      providers: [ToastService]
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update filteredProducts when updateFilteredProducts is called', () => {
+    const mockProducts = [
+      {
+        id: 1,
+        name: 'Produto 1',
+        description: 'Descrição do Produto 1',
+        price_in_cents: 1000,
+        image_url: 'https://example.com/produto1.jpg'
+      }
+    ];
+    component.updateFilteredProducts(mockProducts);
+    expect(component.filteredProducts).toEqual(mockProducts);
   });
 });
